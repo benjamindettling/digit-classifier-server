0e501a51dd3922d4830f0fcabe8c030fc5f6589f
bdettling <bdettling@student.ethz.ch>
2025-04-07 23:28:04 +0200

    adjust model for bigger NN, 99.60%

--- app/model.py
+++ app/model.py
@@ -2,20 +2,32 @@ import torch
 import torch.nn as nn
 from torchvision.transforms import ToTensor, Normalize
 
-class MNIST_CNN(nn.Module):
+class BiggerMNISTCNN(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = nn.Sequential(
-            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.Conv2d(1, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
-            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(2, 2),
+
             nn.Flatten(),
-            nn.Linear(64 * 7 * 7, 128),
+            nn.Linear(128 * 7 * 7, 512),
             nn.ReLU(),
-            nn.Dropout(0.2),
+            nn.Dropout(0.5),
+            nn.Linear(512, 128),
+            nn.ReLU(),
             nn.Linear(128, 10)
         )
 
@@ -24,7 +36,7 @@ class MNIST_CNN(nn.Module):
 
 def load_model(path="mnist_model.pth"):
     device = "cuda" if torch.cuda.is_available() else "cpu"
-    model = MNIST_CNN()
+    model = BiggerMNISTCNN()
     checkpoint = torch.load(path, map_location=device)
     model.load_state_dict(checkpoint["model_state_dict"])
     model.to(device).eval()
@@ -32,9 +44,9 @@ def load_model(path="mnist_model.pth"):
 
 def predict_digit(model, image):
     device = next(model.parameters()).device
-    tensor = ToTensor()(image)
+    tensor = ToTensor()(image).unsqueeze(0).to(device)  # shape: [1, 1, 28, 28]
     tensor = Normalize((0.1307,), (0.3081,))(tensor)
-    tensor = tensor.unsqueeze(0).to(device)  # shape [1, 1, 28, 28]
+
     with torch.no_grad():
         output = model(tensor)
         return output.argmax(dim=1).item()
