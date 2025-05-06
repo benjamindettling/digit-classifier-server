67086f85d6ae58d6f35f209dcd12fb8a09bfe1be
bdettling <bdettling@student.ethz.ch>
2025-05-06 14:59:45 +0200

    allow all origins

--- app/main.py
+++ app/main.py
@@ -9,8 +9,7 @@ app = FastAPI()
 # CORS settings (adjust if needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://digit.benjamindettling.ch",
-                   "http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
